cea5323d20d851513679b91c6e810bf2b46000ac
antisdream <jiyong8074@gmail.com>
2026-06-24 21:00:40 +0900

    2026-06-24|AI 활용 애플리케이션 개발-Django 프로젝트와 Todo CRUD

---
+++ 4. AI Application Development/day71/todos/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    # 폼 검사시 값이 비어있어도 허용
+    content = models.TextField(blank=True)
+    is_completed = models.BooleanField(default=False)
+    # 최초 레코드(row) 가 생성될때(insert) 자동으로 현재시간 등록
+    created_at = models.DateTimeField(auto_now_add=True)
+    # 수정(save / update)때마다 현재시간 자동 갱신
+    updated_at = models.DateTimeField(auto_now=True)
+    #객체를 문자열로 표현할때 기본 출력값을 지정
+    def __str__(self):
+        return f'id:{self.id} title:{self.title} content:{self.content} is_completed:{self.is_completed}\
+        created_at:{self.created_at} updated_at:{self.updated_at}'
---
+++ 4. AI Application Development/day71/todos/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'todos'
+
+urlpatterns = [
+    path('', views.todo_list, name='todo_list'),
+    path('create/', views.todo_create, name='todo_create'),
+    path('<int:pk>/update/', views.todo_update, name='todo_update'),
+    path('<int:pk>/delete/', views.todo_delete, name='todo_delete'),
+    path('about/', views.AboutView.as_view(), name='about'),
+]
